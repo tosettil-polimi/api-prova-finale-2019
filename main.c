4b7e60b7b60d9d3437fa237928bee915b4850485
tosettil <lorenzo.tosetti@mail.polimi.it>
2019-07-08 20:21:50 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Primo test di struttura dati
+typedef struct {
+    char *nome;
+    struct Utente *relazioni;
+} Utente;
+
+void freeUtente(Utente *utente) {
+    free(utente->nome);
+    free(utente->relazioni);
+}
+
+void main() {
+    Utente prova;
+    struct Utente *utenti;
+    
+    prova.nome = malloc(sizeof(char) * 10);
+    utenti = malloc(sizeof(Utente) * 100);
+
+    prova.relazioni = utenti;
+
+    printf("size: %d\n", sizeof(prova.relazioni));
+
+    freeUtente(prova);
+}
